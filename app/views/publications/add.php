5e03d9fadb1052e6bad9a0f1ea99e065131ea867
MedHachami <moha.hach.9@gmail.com>
2023-11-15 15:16:06 +0100

    Create add.php

---
+++ app/views/publications/add.php
@@ -0,0 +1,14 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+<div class="app-container">
+    <div class="sidebar-container">
+        <ul class="sidebar-items">
+            <li class="sidebar-item"><img src="<?php echo URLROOT; ?>/img/home-icon.png" ><a href="<?php echo URLROOT; ?>/" class="sidebar-link">Home</a></li>
+            <li class="sidebar-item"><img src="<?php echo URLROOT; ?>/img/add-icon.png" ><a href="<?php echo URLROOT; ?>/publications/add" class="sidebar-link">Add Publication</a></li>
+            <li class="sidebar-item"><img src="<?php echo URLROOT; ?>/img/users-icon.png" ><a href="" class="sidebar-link">View User</a></li>
+        </ul>
+    </div>
+    
+
+</div>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
+
