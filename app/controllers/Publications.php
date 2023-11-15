495240316615cf5af749bbf947b2db448426c6a1
MedHachami <moha.hach.9@gmail.com>
2023-11-15 15:13:22 +0100

    get publication from the model

--- app/controllers/Publications.php
+++ app/controllers/Publications.php
@@ -2,17 +2,31 @@
   class Publications extends Controller {
 
     public function __construct(){
+       if(!isLoggedIn()){
+        redirect('users/login');
+          
+       }
+       $this->publicationModel = $this->model('Publication');
+        $this->userModel = $this->model('User');
        
-        $this->publicationModel = $this->model('Publication');
     }
 
     public function index(){
-      
+      $publications = $this->publicationModel->getPublications();
       $data = [
-        'title' => 'publications',
+        'publications' => $publications
       ];
 
       $this->view('publications/index', $data);
+    }
+
+    public function add(){
+      if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        // Sanitize POST array
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+        
+      }
     }
 
     
