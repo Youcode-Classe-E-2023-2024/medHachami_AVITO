44ad6870ead101a8a83d7fc1d0c83a9fe5c32510
MedHachami <moha.hach.9@gmail.com>
2023-11-15 15:13:40 +0100

    create getPublications in model

--- app/models/Publication.php
+++ app/models/Publication.php
@@ -7,5 +7,21 @@ class Publication{
         
     }
 
+    public function getPublications(){
+        $this->db->query('SELECT publications.*, 
+                             users.id as user_id,
+                             users.name as user_name,
+                             users.email as user_email,
+                             users.city as user_city,
+                             users.imgUrl as user_img
+                      FROM publications
+                      INNER JOIN users
+                      ON publications.user_id = users.id
+                      ORDER BY publications.created_at DESC');
+
+        $results = $this->db->resultSet();
+
+        return $results;
+    }
 
 }
