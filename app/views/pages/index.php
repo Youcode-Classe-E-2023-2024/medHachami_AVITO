f4e976836bb84c1d3bc172d697a6428da23825cf
MedHachami <moha.hach.9@gmail.com>
2023-11-15 15:15:35 +0100

    display pubs in views

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -2,63 +2,12 @@
 <div class="app-container">
     <div class="sidebar-container">
         <ul class="sidebar-items">
-            <li class="sidebar-item"><img src="<?php echo URLROOT; ?>/img/home-icon.png" ><a href="" class="sidebar-link">Home</a></li>
-            <li class="sidebar-item"><img src="<?php echo URLROOT; ?>/img/add-icon.png" ><a href="" class="sidebar-link">Add Publication</a></li>
+            <li class="sidebar-item"><img src="<?php echo URLROOT; ?>/img/home-icon.png" ><a href="<?php echo URLROOT; ?>/" class="sidebar-link">Home</a></li>
+            <li class="sidebar-item"><img src="<?php echo URLROOT; ?>/img/add-icon.png" ><a href="<?php echo URLROOT; ?>/publications/add" class="sidebar-link">Add Publication</a></li>
             <li class="sidebar-item"><img src="<?php echo URLROOT; ?>/img/users-icon.png" ><a href="" class="sidebar-link">View User</a></li>
         </ul>
     </div>
-    <div class="publications-container">
-        <div class="pub-card">
-            <div class="pub-header">
-                <div class="user-img">
-                    <img src="<?php echo URLROOT; ?>/img/med hachami.jpg" >
-                </div>
-                <div class="user-details">
-                    <p class="user-name1">Mohamed Hachami</p>
-                    <p class="user-city">Marrakech</p>
-                </div>
-            </div>
-            <div class="thumb" style="" >
-                <img src="<?php echo URLROOT; ?>/img/teambg.jpeg" alt="" class="" style="width: 100%;" >
-            </div>
-            <div class="pub-footer">
-                <img src="<?php echo URLROOT; ?>/img/heart-icon.png" alt="" >
-                <div class="row1">
-                    <p class="user-name2">MedHachami</p>
-                    <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elits.amet consectetur adipisicing elits. </p>
-                </div>
-                
-            </div>
-            
-        </div>
-        <div class="pub-card">
-            <div class="pub-header">
-                <div class="user-img">
-                    <img src="<?php echo URLROOT; ?>/img/med hachami.jpg" >
-                </div>
-                <div class="user-details">
-                    <p class="user-name1">Mohamed Hachami</p>
-                    <p class="user-city">Marrakech</p>
-                </div>
-            </div>
-            <div class="thumb" style="" >
-                <img src="<?php echo URLROOT; ?>/img/teambg.jpeg" alt="" class="" style="width: 100%;" >
-            </div>
-            <div class="pub-footer">
-                <img src="<?php echo URLROOT; ?>/img/heart-icon.png" alt="" >
-                <div class="row1">
-                    <p class="user-name2">MedHachami</p>
-                    <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elits. </p>
-                </div>
-                
-            </div>
-            
-        </div>
-        
-    </div>
-    <div class="users-container">
-        <h1>right</h1>
-    </div>
+    
 
 </div>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
