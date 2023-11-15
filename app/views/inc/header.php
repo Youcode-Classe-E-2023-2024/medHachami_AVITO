7837ded228e3e088dbaec41c0efa3e6f9b4d2298
MedHachami <moha.hach.9@gmail.com>
2023-11-15 02:30:14 +0100

    Update header.php

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -4,8 +4,25 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
   <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
-  <title><?php echo $data['title']; ?></title>
+  <title><?php echo SITENAME; ?></title>
 </head>
+<nav class="navbar-container">
+    <div class="left">
+      <h2 class="logo">Hachami_Avito </h2>
+    </div>
+    <div class="right">
+      <?php if(isset($_SESSION['user_id'])) : ?>
+          <a class="hert-navbar"><img src="<?php echo URLROOT; ?>/img/heart-icon.png" alt="" srcset=""></a>
+          <a class="login-btn" href="<?php echo URLROOT; ?>/users/logout">Logout</a>
+      <?php else : ?>
+      <a class="sign-btn" href="<?php echo URLROOT; ?>/users/register">Sign In</a>
+      <a class="login-btn">Login In</a>
+      <?php endif; ?>
+    </div>
+    
+</nav>
 <body>
   
