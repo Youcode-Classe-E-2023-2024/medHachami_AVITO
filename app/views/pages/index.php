f4fd7a71ad2e372ce519a302e3cd1a4d418e078a
MedHachami <moha.hach.9@gmail.com>
2023-11-15 02:30:46 +0100

    Update index.php

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,4 +1,32 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
- 
+<div class="app-container">
+    <div class="sidebar-container">
+        <ul class="sidebar-items">
+            <li class="sidebar-item"><img src="<?php echo URLROOT; ?>/img/home-icon.png" ><a href="" class="sidebar-link">Home</a></li>
+            <li class="sidebar-item"><img src="<?php echo URLROOT; ?>/img/add-icon.png" ><a href="" class="sidebar-link">Add Publication</a></li>
+            <li class="sidebar-item"><img src="<?php echo URLROOT; ?>/img/users-icon.png" ><a href="" class="sidebar-link">View User</a></li>
+        </ul>
+    </div>
+    <div class="publications-container">
+        <div class="pub-card">
+            <div class="pub-header">
+                <div class="user-img">
+                    <img src="" alt="" srcset="">
+                </div>
+                <div class="user-details">
+                    <p class="user-name">Mohamed Hachami</p>
+                    <p class="user-city">Marrakech</p>
+                </div>
+            </div>
+            <div class="pub-">
+
+            </div>
+        </div>
+    </div>
+    <div class="users-container">
+        <h1>right</h1>
+    </div>
+
+</div>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
 
