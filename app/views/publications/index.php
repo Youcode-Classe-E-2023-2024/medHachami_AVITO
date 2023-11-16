dc30a4d92513ddd3a93001cbab960ef7c583aea3
MedHachami <moha.hach.9@gmail.com>
2023-11-16 01:07:17 +0100

    Update index.php

--- app/views/publications/index.php
+++ app/views/publications/index.php
@@ -8,11 +8,13 @@
         </ul>
     </div>
     <div class="publications-container">
+        
         <?php foreach($data['publications'] as $pub) : ?>
         <div class="pub-card">
             <div class="pub-header">
                 <div class="user-img">
-                    <img src="<?php echo URLROOT; ?>/img/med hachami.jpg" >
+                    <img src="<?php echo URLROOT. "/img/" . $pub->user_img ; ?>" >
+                    
                 </div>
                 <div class="user-details">
                     <p class="user-name1"><?php echo $pub->user_name;  ?></p>
@@ -20,14 +22,16 @@
                 </div>
             </div>
             <div class="thumb" style="" >
-                <img src="<?php echo URLROOT; ?>/img/teambg.jpeg" alt="" class="" style="width: 100%;" >
+                <img src="<?php echo URLROOT . "/img/" . $pub->pub_img; ?>" class="" style="width: 100%;" alt="Publication Image">
             </div>
             <div class="pub-footer">
                 <img src="<?php echo URLROOT; ?>/img/heart-icon.png" alt="" >
                 <div class="row1">
                     <p class="user-name2"><?php echo $pub->user_name; ?></p>
-                    <p class="description">Lorem ipsum dolor sit amet consectetur adipisicing elits.amet consectetur adipisicing elits. </p>
+                    <p class="description"><?php echo $pub->pub_description; ?></p>
+                    
                 </div>
+                <p class="timeDiff"><?php echo $pub->diffTime; ?></p>
                 
             </div>
             
@@ -39,20 +43,17 @@
     </div>
     <div class="users-container">
         <h3>Discrover users</h3>
+        <?php foreach($data['users'] as $user) : ?>
         <div class="user-row">
                 <div class="user-img">
-                    <img src="<?php echo URLROOT; ?>/img/med hachami.jpg" >
+                    <img src="<?php echo URLROOT . "/img/" . $user->imgUrl; ?>" >
                 </div>
-                <h2>MedHachami</h2>
+                <h2><?php echo $user->name; ?></h2>
         </div>
-        <div class="user-row">
-                <div class="user-img">
-                    <img src="<?php echo URLROOT; ?>/img/med hachami.jpg" >
-                </div>
-                <h2>MedHachami</h2>
-        </div>
+        
+        <?php endforeach; ?>
     </div>
-
+    
 </div>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
 
