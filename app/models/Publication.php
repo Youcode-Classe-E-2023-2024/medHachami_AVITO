bfb67730903d7a5c4b07130b66013da339cce6cb
MedHachami <moha.hach.9@gmail.com>
2023-11-16 01:07:00 +0100

    Update Publication.php

--- app/models/Publication.php
+++ app/models/Publication.php
@@ -8,7 +8,9 @@ class Publication{
     }
 
     public function getPublications(){
-        $this->db->query('SELECT publications.*, 
+        $this->db->query('SELECT publications.imgUrl as pub_img,
+                            publications.description as pub_description,
+                            publications.created_at as pub_created_at, 
                              users.id as user_id,
                              users.name as user_name,
                              users.email as user_email,
@@ -22,6 +24,26 @@ class Publication{
         $results = $this->db->resultSet();
 
         return $results;
+    }
+
+    public function addPub($data){
+        $this->db->query('INSERT INTO publications (title, description, price, imgUrl, created_at, user_id) VALUES (:title, :description, :price, :imgUrl, :created_at, :user_id)');
+
+        // Bind values
+        $this->db->bind(':title', $data['title']);
+        $this->db->bind(':description', $data['description']);
+        $this->db->bind(':price', $data['price']);
+        
+        
+        $this->db->bind(':imgUrl', $data['image']);
+
+        // Set the current time
+        $this->db->bind(':created_at', date('Y-m-d H:i:s'));
+
+        $this->db->bind(':user_id', $data['user_id']);
+
+        // Execute
+        return $this->db->execute();
     }
 
 }
