c0ed920fa00029f3ae9be9c81a767199612d1b8b
MedHachami <moha.hach.9@gmail.com>
2023-11-17 16:07:04 +0100

    Update add.php

--- app/views/publications/add.php
+++ app/views/publications/add.php
@@ -4,8 +4,7 @@
         <ul class="sidebar-items">
             <li class="sidebar-item"><img src="<?php echo URLROOT; ?>/img/home-icon.png" ><a href="<?php echo URLROOT; ?>/" class="sidebar-link">Home</a></li>
             <li class="sidebar-item"><img src="<?php echo URLROOT; ?>/img/add-icon.png" ><a href="<?php echo URLROOT; ?>/publications/add" class="sidebar-link">Add Publication</a></li>
-            <li class="sidebar-item"><img src="<?php echo URLROOT; ?>/img/users-icon.png" ><a href="" class="sidebar-link">View User</a></li>
-        </ul>
+            <li class="sidebar-item"><img src="<?php echo URLROOT; ?>/img/books-icon.png" ><a href="<?php echo URLROOT; ?>/publications/myPublication" class="sidebar-link">My publications</a></li>        </ul>
     </div>
     <div class="publications-container">
             <?php if(isset($_GET['action']) ){?>
