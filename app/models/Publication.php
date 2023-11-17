0a4c5ee41489faac54b0c0c0e46c8d0ebfd50887
MedHachami <moha.hach.9@gmail.com>
2023-11-17 16:06:54 +0100

    Update Publication.php

--- app/models/Publication.php
+++ app/models/Publication.php
@@ -8,7 +8,8 @@ class Publication{
     }
 
     public function getPublications(){
-        $this->db->query('SELECT publications.imgUrl as pub_img,
+        $this->db->query('SELECT publications.id as pub_id,
+                             publications.imgUrl as pub_img,
                             publications.description as pub_description,
                             publications.created_at as pub_created_at, 
                              users.id as user_id,
@@ -21,6 +22,27 @@ class Publication{
                       ON publications.user_id = users.id
                       ORDER BY publications.created_at DESC');
 
+        $results = $this->db->resultSet();
+
+        return $results;
+    }
+    public function getMyPublications(){
+        $this->db->query('SELECT
+                     publications.id as pub_id,
+                             publications.imgUrl as pub_img,
+                            publications.description as pub_description,
+                            publications.created_at as pub_created_at, 
+                             users.id as user_id,
+                             users.name as user_name,
+                             users.email as user_email,
+                             users.city as user_city,
+                             users.imgUrl as user_img
+                             FROM publications
+                      INNER JOIN users
+                      ON publications.user_id = users.id
+                      WHERE user_id = :user_id
+                      ORDER BY publications.created_at DESC');
+        $this->db->bind(':user_id', $_SESSION['user_id']);
         $results = $this->db->resultSet();
 
         return $results;
@@ -44,6 +66,48 @@ class Publication{
 
         // Execute
         return $this->db->execute();
+    }
+
+    public function addLike($userId, $publicationId) {
+        $this->db->query('SELECT * FROM likes WHERE user_id = :user_id AND publication_id = :publication_id');
+        $this->db->bind(':user_id', $userId);
+        $this->db->bind(':publication_id', $publicationId);
+        
+        $existingLike = $this->db->single();
+        if(!$existingLike){
+            $this->db->query('INSERT INTO likes (user_id, publication_id) VALUES (:user_id, :publication_id)');
+            $this->db->bind(':user_id', $userId);
+            $this->db->bind(':publication_id', $publicationId);
+            $this->db->execute();
+            return 1;
+        }else{
+            $this->db->query('DELETE FROM likes WHERE user_id = :user_id AND publication_id = :publication_id');
+            $this->db->bind(':user_id', $userId);
+            $this->db->bind(':publication_id', $publicationId);
+        
+            $this->db->execute();
+            return 0;
+        }
+    
+        
+    }
+    // public function dislike($userId, $publicationId) {
+       
+    // }
+
+
+    public function hasLiked($publicationId,$userId) {
+        $this->db->query('SELECT * FROM likes WHERE user_id = :user_id AND publication_id = :publication_id');
+        $this->db->bind(':user_id', $userId);
+        $this->db->bind(':publication_id', $publicationId);
+    
+        $results = $this->db->resultSet();
+        
+    
+        $result = count($results) > 0;
+        
+    
+        return $result;
     }
 
 }
